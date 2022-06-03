94120ca9ffdac0e67e614c223924a07eb375d4db
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2022-06-03 14:23:47 +0200

    adding the pages to alex province

---
+++ .history/pages/provinces/alexandra/bihira/kafr-edawar/index_20220603142213.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+
+// Custom Components
+import Header from "../../../../../components/Header";
+import PageContainer from "../../../../../components/PageContainer";
+import MainContentArea from "../../../../../components/MainContentArea";
+
+// Constants.
+import provincesList from "../../../../../constants/provincesList";
+import mainAreaCoords from "../../../../../constants/provinces/mainArea";
+
+// Images Sources.
+import mainAreaImage from "../../../images/provinces/alexandra/alex_region.jpg";
+
+const AlexProvince: NextPage = () => {
+  return (
+    <PageContainer>
+      <Header headerTitle="اقليم الاسكندرية" />
+      <MainContentArea
+        list={provincesList}
+        mapAreas={mainAreaCoords}
+        mapImageSrc={mainAreaImage.src}
+        mapImageWidth={235}
+        mapImageHeight={166.383}
+        quickNavTitle="اقاليم مصر"
+      />
+    </PageContainer>
+  );
+};
+
+export default AlexProvince;
---
+++ .history/pages/provinces/alexandra/bihira/kafr-edawar/index_20220603142228.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from "next";
+
+// Custom Components
+import Header from "../../../../../components/Header";
+import PageContainer from "../../../../../components/PageContainer";
+import MainContentArea from "../../../../../components/MainContentArea";
+
+// Constants.
+import provincesList from "../../../../../constants/provincesList";
+import mainAreaCoords from "../../../../../constants/provinces/mainArea";
+
+// Images Sources.
+import mainAreaImage from "../../../images/provinces/alexandra/alex_region.jpg";
+
+const AlexProvince: NextPage = () => {
+  return (
+    <PageContainer>
+      <Header headerTitle="اقليم الاسكندرية" />
+      <MainContentArea
+      breadcrumbItems={[]}
+        list={provincesList}
+        mapAreas={mainAreaCoords}
+        mapImageSrc={mainAreaImage.src}
+        mapImageWidth={235}
+        mapImageHeight={166.383}
+        quickNavTitle="اقاليم مصر"
+      />
+    </PageContainer>
+  );
+};
+
+export default AlexProvince;
