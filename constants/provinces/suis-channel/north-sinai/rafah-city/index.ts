d4a532cc8ee4bc7038e7e5abb48d1e6ea2676dfb
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2022-06-05 21:30:46 +0200

    adding the pages

---
+++ constants/provinces/suis-channel/north-sinai/rafah-city/index.ts
@@ -0,0 +1,26 @@
+import { BreadcrumbElement } from "components/MainContentArea/MapArea/MapInfoDisplayedLinks/MapInfoDisplayedLinks";
+
+const list: Array<BreadcrumbElement> = [
+  {
+    text: "مركز الحمام",
+    url: "#",
+  },
+  {
+    text: "ساحل العميد",
+    url: "#",
+  },
+  {
+    text: "السلام",
+    url: "#",
+  },
+  {
+    text: "العميد",
+    url: "#",
+  },
+  {
+    text: "اولاد مسعود",
+    url: "#",
+  },
+];
+
+export default list;
