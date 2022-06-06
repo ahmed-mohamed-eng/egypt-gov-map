dc22b2571d97146095a7da421c6f39e0d844aef3
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2022-06-06 16:09:23 +0200

    fixing errors

--- constants/provinces/delta/dakhlia/elmataria/index.ts
+++ constants/provinces/delta/dakhlia/elmataria/index.ts
@@ -1,3 +1,5 @@
 import { BreadcrumbElement } from "components/MainContentArea/MapArea/MapInfoDisplayedLinks/MapInfoDisplayedLinks";
 
 const list: Array<BreadcrumbElement> = [];
+
+export default list;
